b3e1ca72b13453b2be4aea7813ff5049715d68e5
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2022-06-18 03:53:22 +0800

    adding nav menu in hamburger nav

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,14 +1,14 @@
 // hamburger nav anim
 
 const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
 });
 
 // navbar fixed
-
-
 window.onscroll = () => {
     const header = document.querySelector('header');
     this.scrollY > 20 ? header.classList.add('navbar-fixed') : header.classList.remove('navbar-fixed');
