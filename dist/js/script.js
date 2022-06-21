ab43a9f0283ef0ee7e8633eb9dd1adb6894b71b9
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2022-06-22 03:55:20 +0800

    remove smooth scroll and adding back to top script

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,15 +14,26 @@ window.onscroll = () => {
     this.scrollY > 20 ? header.classList.add('navbar-fixed') : header.classList.remove('navbar-fixed');
 }
 
-// smooth scroll
-const goHome = document.querySelector('#home');
-const goAbout = document.querySelector('#about');
-const goSkills = document.querySelector('#skills');
-const goPorto = document.querySelector('#portofolio');
-const goContact = document.querySelector('#contact');
+// Back to Top Button
+const mybutton = document.getElementById("btn-back-to-top");
 
-goHome.scrollIntoView({ behavior: 'smooth' });
-goAbout.scrollIntoView({ behavior: 'smooth' });
-goSkills.scrollIntoView({ behavior: 'smooth' });
-goPorto.scrollIntoView({ behavior: 'smooth' });
-goContact.scrollIntoView({ behavior: 'smooth' });
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = () => {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } 
+    else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
